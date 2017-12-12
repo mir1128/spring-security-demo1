431c95eb4168307cda1c27ea7a13533860f123e3
jie.liu <jieliu@thoughtworks.com>
2017-12-12 15:43:30 +0800

    add syntax error. --liujie

--- src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
@@ -11,6 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        121
         auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
     }
 }
